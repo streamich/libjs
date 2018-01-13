ffac15f617eb6378a10a81f9d05fc97f21056515
Vadim Dalecky <streamich@gmail.com>
2018-01-13 22:11:25 +0000

    chore: add build process

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+
+gulp.task('build-ts', () => {
+    return gulp
+        .src(['src/**/*.ts', '!src/**/__tests__/**'])
+        .pipe(
+            ts({
+                target: 'es5',
+                module: 'commonjs',
+                removeComments: false,
+                noImplicitAny: false,
+                sourceMap: false,
+            })
+        )
+        .pipe(gulp.dest('lib'));
+});
